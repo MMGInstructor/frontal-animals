34babfd86ede4ef9fcce30526bb2de3e79ebf49e
MartaMG <59799120+MMGInstructor@users.noreply.github.com>
2026-03-06 19:36:22 +0100

    Add files via upload

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-$host = getenv("MYSQL_SERVICE_HOST") ?: "localhost";
+$host = getenv("MYSQL_SERVICE_HOST");
 $dbname = "animals";
 $user = "datawriter";
 $password = "write123";
@@ -9,12 +9,7 @@ try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-
-    // escribir error en los logs
-    error_log("ERROR: No se pudo conectar a la base de datos MySQL en host '$host'. Detalle: " . $e->getMessage());
-
-    // mensaje genérico al usuario
-    die("Error interno de la aplicación.");
+    die("Error de conexión: " . $e->getMessage());
 }
 
 ?>
